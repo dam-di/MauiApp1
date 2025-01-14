d65462e0e5da037d2626312d57e9968a4d80eb21
user-dam <dam@dam.es>
2025-01-14 21:23:54 +0100

    Filtrar por correo

---
+++ MauiApp1/ViewModels/ReportsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModels
+{
+    public partial class ReportsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string urlPDF; // Para source del webview (endpoint)
+
+        [ObservableProperty]
+        private string filtroCorreo; // Capturar el texto del correo
+
+        [RelayCommand]
+        public void GetPDF()
+        {
+            UrlPDF = "http://localhost:8080/report/getReport2/" + FiltroCorreo;
+        }
+
+
+
+    }
+}
